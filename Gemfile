2e4a9667012a6d49930aeccb21e3ec4ce0d032b5
Michael Ho <chacharoti@gmail.com>
2020-01-15 00:22:06 -0800

    Active Admin Init

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'jquery-rails'
 gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
 gem 'devise'
 gem 'kaminari'
+gem 'activeadmin'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+AdminUser.create(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 
 (1..100).each do |count|
   break if Movie.all.size > 99
