640796ad5848ea71edadaf97afaa0403e80be02e
Michael Ho <chacharoti@gmail.com>
2020-01-14 23:40:12 -0800

    Add rating bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,19 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+
+$(function() {
+  if($('.movie-rating').length > 0 ){
+    $.each($('.movie-rating'), function( index, value ) {
+      $(value).rateYo({
+        starWidth: "20px",
+        numStars: 5,
+        halfStar: true,
+        onSet: function (rating, rateYoInstance) {
+          byebug
+          console.log(rating);
+        }
+      });
+    });
+  }
+});
